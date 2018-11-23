75e411c752b30322256e84246c3a72db66421aec
daithanh2910 <daithanh2910@outlook.com>
2018-11-23 20:00:50 +0700

    Update

--- ComiShop/Areas/Admin/Controllers/ReportController.cs
+++ ComiShop/Areas/Admin/Controllers/ReportController.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComiShop.Interfaces;
+using ComiShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComiShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class ReportController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReportController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var warehouseReport = WarehouseReport();
+            return View(warehouseReport);
+        }
+
+        private List<CategoryWarehouse> WarehouseReport()
+        {
+            var productList = _unitOfWork.ProductRepository.GetAll().Include(x => x.Category)
+                .Where(x => x.Deleted == false).Select(x => new Product {
+                    Id = x.Id,
+                    CategoryId = x.CategoryId,
+                    Quantity = x.Quantity,
+                    UnitPrice = x.UnitPrice,
+                    Category = new Category
+                    {
+                        Id = x.Category.Id,
+                        CategoryName = x.Category.CategoryName
+                    }
+                })
+                .GroupBy(x => x.CategoryId).ToList();
+
+            List<CategoryWarehouse> categoryWarehouses = new List<CategoryWarehouse>();
+
+            foreach(var product in productList)
+            {
+                CategoryWarehouse categoryWarehouse = new CategoryWarehouse
+                {
+                    CategoryName = product.First().Category.CategoryName,
+                    TotalQuantity = product.Sum(x => x.Quantity),
+                };
+
+                categoryWarehouses.Add(categoryWarehouse);
+            }
+
+            return categoryWarehouses;
         }
     }
 }
---
+++ ComiShop/ViewModels/CategoryWarehouse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComiShop.ViewModels
+{
+    public class CategoryWarehouse
+    {
+        public string CategoryName { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
